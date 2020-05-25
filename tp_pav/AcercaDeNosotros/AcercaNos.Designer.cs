655145fb8ed207730a64425fad57b7275d62f089
TotomiEcio <63461656+TotomiEcio@users.noreply.github.com>
2020-05-25 18:49:54 -0300

    Add files via upload

---
+++ tp_pav/AcercaDeNosotros/AcercaNos.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Juventus.AcercaDeNosotros
+{
+    partial class AcercaNos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AcercaNos));
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(0, 0);
+            this.lblInfo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(0, 24);
+            this.lblInfo.TabIndex = 0;
+            // 
+            // AcercaNos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(1028, 557);
+            this.Controls.Add(this.lblInfo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "AcercaNos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Acerca de nosotros";
+            this.Load += new System.EventHandler(this.AcercaNos_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
---
+++ tp_pav/AcercaDeNosotros/AcercaNos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Juventus.AcercaDeNosotros
+{
+    public partial class AcercaNos : Form
+    {
+        private string info = "Ateneo Juventus es una asociación civil sin fines de lucro (Asociación Civil Ateneo Juventus - Cuit 30-71053858-8),\n que tiene por objetivo la formación de jóvenes dirigentes cristianos, al estilo de Jesús,\n para que vivan y transmitan el Evangelio siendo “sal y luz” en el mundo, en su ámbito natural de acción." +
+            "\n Fue Fundado por el Padre Capuchino Carmelo d´Agostino\n el 26 de Abril de 1958. Hoy, a pesar del fallecimiento de su fundador,\n la Institución sigue contando con miles de ateneístas que están comprometidos en el sostenimiento de este espacio,\n que permite a los adolescentes formar su proyecto personal de vida aprendiendo a caminar con Dios en su vida, acompañados de otros jóvenes,\n vacuna eficaz contra el vacío y la soledad." +
+            "El proceso formativo comienza desde muy temprano y de\n diferentes maneras participan chicos desde 4º grado hasta la Universidad\n y nos acompaña luego durante toda nuestra vida.Son los mismos chicos quienes van asumiendo, al crecer,\n el rol de formadores de quienes ingresan,\n y participan más tarde como padres respaldando el trabajo de los jefes." +
+            "\n Una parte importante de la formación de los jóvenes consiste en la acción social y la ayuda al prójimo.\n Desde hace unos años Ateneo trabaja de forma activa y permanente en distintos barrios de la ciudad, asilos, hogares, etc";
+
+        public AcercaNos()
+        {
+            InitializeComponent();
+        }
+
+        private void AcercaNos_Load(object sender, EventArgs e)
+        {
+            lblInfo.Text = info;
+        }
+    }
+}
